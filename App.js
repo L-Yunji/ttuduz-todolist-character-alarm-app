c538fbda13cd6e17cd0c58ace4f8203f27393eca
Jungeunkim-dev <67693142+Jungeunkim-dev@users.noreply.github.com>
2021-02-06 02:19:45 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
-import { Modal, StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Button, Alert, YellowBox, TouchableHighlight } from 'react-native';
+import { ScrollView, Modal, StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Button, Alert, YellowBox, TouchableHighlight } from 'react-native';
 import Carousel from 'react-native-carousel-control';
+import Feather from 'react-native-vector-icons/Feather';
 
 //20200205 modified
 export default class App extends React.Component {
 
-  state={
-    isVisible:false
+  state = {
+    isVisible: false
   };
 
-  displayModal(show){
-    this.setState({isVisible:show})
+  displayModal(show) {
+    this.setState({ isVisible: show })
   }
+
+
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text>네비게이터 적용해 줄 공간입니다.</Text>
+          <Text>네비게이션 부분입니다.</Text>
+          
         </View>
         <View style={styles.circleindicator}>
 
@@ -25,50 +29,52 @@ export default class App extends React.Component {
           <TouchableOpacity onPress={() => Alert.alert('캐릭터')}
             style={styles.characterbutton}>
             <Image
-              source={{
-                uri: 'https://i.pinimg.com/originals/84/88/4d/84884d4d70ed594f8da917371a5fa6bb.gif'
-              }}
-              style={{ width: 40, height: 40, borderRadius: 100 }}
+              source={
+                require('./characterimage/강.png')
+              }
+              style={{ width: 37, height: 30}}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => Alert.alert('캐릭터')}
             style={styles.characterbutton}>
             <Image
-              source={{
-                uri: 'https://i.pinimg.com/originals/84/88/4d/84884d4d70ed594f8da917371a5fa6bb.gif'
-              }}
-              style={{ width: 40, height: 40, borderRadius: 100 }}
+              source={
+                require('./characterimage/수.png')
+              }
+              style={{ width: 37, height: 30}}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => Alert.alert('캐릭터')}
             style={styles.characterbutton}>
             <Image
-              source={{
-                uri: 'https://i.pinimg.com/originals/84/88/4d/84884d4d70ed594f8da917371a5fa6bb.gif'
-              }}
-              style={{ width: 40, height: 40, borderRadius: 100 }}
+              source={
+                require('./characterimage/토원본귀.png')
+              }
+              style={{ width: 37, height: 30}}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => Alert.alert('캐릭터')}
             style={styles.characterbutton}>
             <Image
-              source={{
-                uri: 'https://i.pinimg.com/originals/84/88/4d/84884d4d70ed594f8da917371a5fa6bb.gif'
-              }}
-              style={{ width: 40, height: 40, borderRadius: 100 }}
+              source={
+                require('./characterimage/판.png')
+              }
+              style={{ width: 37, height: 30}}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => Alert.alert('캐릭터')}
-            style={styles.characteraddbutton}>
+            style={styles.characterbutton}>
             <Image
-              source={{
-                uri: 'https://wallpaperaccess.com/full/1091550.jpg'
-              }}
-              style={{ width: 40, height: 40, borderRadius: 100 }}
+              source={
+                require('./characterimage/펭.png')
+              }
+              style={{ width: 37, height: 30}}
             />
           </TouchableOpacity>
+  
 
         </View>
+        
         <View style={styles.category}>
 
           <TouchableOpacity
@@ -99,10 +105,12 @@ export default class App extends React.Component {
 
 
 
+
         </View>
         <View style={styles.cardview}>
 
-          <Carousel pageStyle={{ backgroundColor: "white", height: 260, width: 188 }}>
+          <Carousel pageStyle={{ backgroundColor: "white", height: 260, width: 188 }}
+          >
             <TouchableOpacity>
               <Image
                 source={{
@@ -128,7 +136,7 @@ export default class App extends React.Component {
                 }}
                 style={{ width: 154, height: 203 }}
               />
-              <Text>몽몽</Text>
+              <Text>몽</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => Alert.alert('모달화면 띄우기')}>
               <Image
@@ -142,57 +150,64 @@ export default class App extends React.Component {
 
 
           </Carousel>
-
           <Text>Indicator자리</Text>
           <TouchableOpacity
             onPress={() => {
               this.displayModal(true);
             }}
             style={styles.categorybutton}>
-            <Text>사전</Text>
+          <Feather name={'book-open'} size={25} color={'black'}/>
           </TouchableOpacity>
 
         </View>
         <View style={styles.navigator}>
           <Text>네비게이터 적용해 줄 공간입니다.</Text>
-          
+
         </View>
 
 
-      <Modal
-      animationType={'fade'}
-      transparent={true}
-      visible={this.state.isVisible}
-      onRequestClose={()=>{
-        Alert.alert('Modal is closed');
-      }}
-      
-      >
-        <View style={{
-          flex:1,
-          flexDirection:'column',
-          justifyContent:'center',
-          alignItems:'center',
-          backgroundColor:'rgba(0,0,0,0.5)'
-        }}>
+        <Modal
+          animationType={'fade'}
+          transparent={true}
+          visible={this.state.isVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal is closed');
+          }}
+
+        >
           <View style={{
-            backgroundColor:'#fff',
-            width:300,
-            height:300,
-            borderRadius:10,
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)'
           }}>
-        <Text>캐릭터 사진</Text>
-        <Text>설명 : 달수리는 물에 사는~~~</Text>
+            <View style={{
+              backgroundColor: '#fff',
+              width: 300,
+              height: 300,
+              borderRadius: 10,
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 4,
+                height: 1,
+              },
+              shadowOpacity: 0.1,
+              shadowRadius: 1.00,
+              elevation: 24,
+            }}>
+              <Text>캐릭터 사진</Text>
+              <Text>설명 : 달수리는 물에 사는~~~</Text>
 
-        <Text onPress={()=>{this.displayModal(!this.state.isVisible);}}>닫기</Text>
+              <Text onPress={() => { this.displayModal(!this.state.isVisible); }}>닫기</Text>
+            </View>
           </View>
-        </View>
 
-      </Modal>
+        </Modal>
 
       </View>
 
-    
+
 
     );
   }
@@ -220,8 +235,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     borderBottomColor: 'black',
-    borderBottomWidth: StyleSheet.hairlineWidth
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    elevation: 2,
   },
+
+
   cardview: {
     height: 390,
     alignItems: 'center',
@@ -283,6 +301,8 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderRadius: 20,
   },
+
+
 
 
 });
